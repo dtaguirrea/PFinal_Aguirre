5a3fe348a1f0145d67ef43b020e84ebb5488f625
Daniel Aguirre <main.data06@gmail.com>
2024-02-04 00:38:27 -0300

    Se agrego el html basico a la pagina, se empieza a trabajar en el script de java, no me logra funcionar sweetalert, en la semana lo intentare solucionar

--- PFinal/script/main.js
+++ PFinal/script/main.js
@@ -5,12 +5,34 @@ let mercaderia =[{"id":1,"nombre":"manzana","precio":10},{"id":2,"nombre":"iphon
 //Carrito
 let carrito=[{"id":1,"cantidad":0},{"id":2,"cantidad":0},{"id":3,"cantidad":0},{"id":4,"cantidad":0},{"id":5,"cantidad":0},{"id":6,"cantidad":0}]
 
-//Queso
-let botonqso=document.getElementById("btnqso")
-botonqso.addEventListener("click",respuestabtnqso)
-function respuestabtnqso(){
-    carrito[5].cantidad=carrito[5].cantidad+1
+//Agregar producto
+function respuestabotonx(numero){
+    carrito.push({id: numero})
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Se agregó al carrito",
+        showConfirmButton: false,
+        timer: 1000
+    });
 }
+//Botones
+let boton1=document.getElementById("boton1")
+boton1.addEventListener("click",    Swal.fire({
+    title: "Su total a pagar es de:",
+    text: precio,
+    confirmoButtonText: "Ir a pagar"
+  }))
+let boton2=document.getElementById("boton2")
+boton2.addEventListener("click",respuestabotonx(2))
+let boton3=document.getElementById("boton3")
+boton3.addEventListener("click",respuestabotonx(3))
+let boton4=document.getElementById("boton4")
+boton4.addEventListener("click",respuestabotonx(4))
+let boton5=document.getElementById("boton5")
+boton5.addEventListener("click",respuestabotonx(5))
+let boton6=document.getElementById("boton6")
+boton6.addEventListener("click",respuestabotonx(6))
 //Vaciar el carrito
 let vaciar=document.getElementById("vaciar")
 vaciar.addEventListener("click",vaciarcarrito)
